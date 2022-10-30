42fc66c9eef97ebaf02a4004fb6adc4d5cb8fac3
NASKO <NASKO@DESKTOP-A38K7V3>
2022-10-30 20:50:25 +0200

    Add project files.

---
+++ UniversalUnitConvertor/Form1.Designer.vb
@@ -0,0 +1,379 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtConvert = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton12 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton11 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton10 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton7 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton8 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton9 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblConvert = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(194, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(589, 40)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Универсален конвертор на единици"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(24, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(389, 24)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Въведете цяла числената стойност тук :"
+        '
+        'txtConvert
+        '
+        Me.txtConvert.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.txtConvert.Location = New System.Drawing.Point(433, 66)
+        Me.txtConvert.Name = "txtConvert"
+        Me.txtConvert.Size = New System.Drawing.Size(100, 29)
+        Me.txtConvert.TabIndex = 2
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Bisque
+        Me.GroupBox1.Controls.Add(Me.RadioButton12)
+        Me.GroupBox1.Controls.Add(Me.RadioButton11)
+        Me.GroupBox1.Controls.Add(Me.RadioButton10)
+        Me.GroupBox1.Controls.Add(Me.RadioButton7)
+        Me.GroupBox1.Controls.Add(Me.RadioButton8)
+        Me.GroupBox1.Controls.Add(Me.RadioButton9)
+        Me.GroupBox1.Controls.Add(Me.RadioButton4)
+        Me.GroupBox1.Controls.Add(Me.RadioButton5)
+        Me.GroupBox1.Controls.Add(Me.RadioButton6)
+        Me.GroupBox1.Controls.Add(Me.RadioButton3)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.GroupBox1.Location = New System.Drawing.Point(24, 120)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(947, 254)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Изберете единица за конвертиране"
+        '
+        'RadioButton12
+        '
+        Me.RadioButton12.AutoSize = True
+        Me.RadioButton12.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.RadioButton12.Location = New System.Drawing.Point(690, 215)
+        Me.RadioButton12.Name = "RadioButton12"
+        Me.RadioButton12.Size = New System.Drawing.Size(207, 26)
+        Me.RadioButton12.TabIndex = 14
+        Me.RadioButton12.TabStop = True
+        Me.RadioButton12.Text = "Сантиметър в метър"
+        Me.RadioButton12.UseVisualStyleBackColor = True
+        '
+        'RadioButton11
+        '
+        Me.RadioButton11.AutoSize = True
+        Me.RadioButton11.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.RadioButton11.Location = New System.Drawing.Point(690, 183)
+        Me.RadioButton11.Name = "RadioButton11"
+        Me.RadioButton11.Size = New System.Drawing.Size(174, 26)
+        Me.RadioButton11.TabIndex = 13
+        Me.RadioButton11.TabStop = True
+        Me.RadioButton11.Text = "Футове в инчове"
+        Me.RadioButton11.UseVisualStyleBackColor = True
+        '
+        'RadioButton10
+        '
+        Me.RadioButton10.AutoSize = True
+        Me.RadioButton10.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.RadioButton10.Location = New System.Drawing.Point(344, 183)
+        Me.RadioButton10.Name = "RadioButton10"
+        Me.RadioButton10.Size = New System.Drawing.Size(203, 26)
+        Me.RadioButton10.TabIndex = 12
+        Me.RadioButton10.TabStop = True
+        Me.RadioButton10.Text = "Милилитър в литър"
+        Me.RadioButton10.UseVisualStyleBackColor = True
+        '
+        'RadioButton7
+        '
+        Me.RadioButton7.AutoSize = True
+        Me.RadioButton7.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.RadioButton7.Location = New System.Drawing.Point(690, 151)
+        Me.RadioButton7.Name = "RadioButton7"
+        Me.RadioButton7.Size = New System.Drawing.Size(186, 26)
+        Me.RadioButton7.TabIndex = 11
+        Me.RadioButton7.TabStop = True
+        Me.RadioButton7.Text = "Метър във футове"
+        Me.RadioButton7.UseVisualStyleBackColor = True
+        '
+        'RadioButton8
+        '
+        Me.RadioButton8.AutoSize = True
+        Me.RadioButton8.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.RadioButton8.Location = New System.Drawing.Point(690, 119)
+        Me.RadioButton8.Name = "RadioButton8"
+        Me.RadioButton8.Size = New System.Drawing.Size(200, 26)
+        Me.RadioButton8.TabIndex = 10
+        Me.RadioButton8.TabStop = True
+        Me.RadioButton8.Text = "Метър в километър"
+        Me.RadioButton8.UseVisualStyleBackColor = True
+        '
+        'RadioButton9
+        '
+        Me.RadioButton9.AutoSize = True
+        Me.RadioButton9.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.RadioButton9.Location = New System.Drawing.Point(690, 87)
+        Me.RadioButton9.Name = "RadioButton9"
+        Me.RadioButton9.Size = New System.Drawing.Size(198, 26)
+        Me.RadioButton9.TabIndex = 9
+        Me.RadioButton9.TabStop = True
+        Me.RadioButton9.Text = "Километър в метър"
+        Me.RadioButton9.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.RadioButton4.Location = New System.Drawing.Point(344, 151)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(201, 26)
+        Me.RadioButton4.TabIndex = 8
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "Литър в милилитър"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.RadioButton5.Location = New System.Drawing.Point(344, 119)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(177, 26)
+        Me.RadioButton5.TabIndex = 7
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Text = "Грам в килограм"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'RadioButton6
+        '
+        Me.RadioButton6.AutoSize = True
+        Me.RadioButton6.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.RadioButton6.Location = New System.Drawing.Point(344, 87)
+        Me.RadioButton6.Name = "RadioButton6"
+        Me.RadioButton6.Size = New System.Drawing.Size(178, 26)
+        Me.RadioButton6.TabIndex = 6
+        Me.RadioButton6.TabStop = True
+        Me.RadioButton6.Text = "Килограм в грам"
+        Me.RadioButton6.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.RadioButton3.Location = New System.Drawing.Point(55, 151)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(175, 26)
+        Me.RadioButton3.TabIndex = 5
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Целзии в келвин"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.RadioButton2.Location = New System.Drawing.Point(55, 119)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(205, 26)
+        Me.RadioButton2.TabIndex = 4
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Фаренхайт в целзий"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.RadioButton1.Location = New System.Drawing.Point(55, 87)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(208, 26)
+        Me.RadioButton1.TabIndex = 3
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Целзий в Фаренхайт"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(755, 49)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(98, 24)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "Дължина"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(418, 49)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(64, 24)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Тегло"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(55, 49)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(130, 24)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Температура"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Times New Roman", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label6.Location = New System.Drawing.Point(79, 410)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(251, 26)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "Конвертиран резултат"
+        '
+        'lblConvert
+        '
+        Me.lblConvert.AutoSize = True
+        Me.lblConvert.Font = New System.Drawing.Font("Times New Roman", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblConvert.Location = New System.Drawing.Point(368, 410)
+        Me.lblConvert.Name = "lblConvert"
+        Me.lblConvert.Size = New System.Drawing.Size(0, 26)
+        Me.lblConvert.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.DarkGray
+        Me.Button1.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(109, 455)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 31)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Изчисти"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.DarkGray
+        Me.Button2.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.Location = New System.Drawing.Point(395, 455)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(138, 31)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Конвертирай"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.DarkGray
+        Me.Button3.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button3.Location = New System.Drawing.Point(750, 455)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(138, 31)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Изход"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Orange
+        Me.ClientSize = New System.Drawing.Size(995, 536)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblConvert)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.txtConvert)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Универсален конвертор за единици"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtConvert As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents RadioButton12 As RadioButton
+    Friend WithEvents RadioButton11 As RadioButton
+    Friend WithEvents RadioButton10 As RadioButton
+    Friend WithEvents RadioButton7 As RadioButton
+    Friend WithEvents RadioButton8 As RadioButton
+    Friend WithEvents RadioButton9 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents RadioButton6 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lblConvert As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ UniversalUnitConvertor/Form1.vb
@@ -0,0 +1,149 @@
+﻿Public Class Form1
+
+    Dim Celsius, Fahrenheit, Kelvin As Double
+    Dim Kilo, Liter, Mililiter, Gram, Centimeter As Double
+
+    Private Sub RadioButton6_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton6.CheckedChanged
+        ioperation = "KG"
+    End Sub
+
+    Private Sub RadioButton5_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton5.CheckedChanged
+        ioperation = "GK"
+    End Sub
+
+    Private Sub RadioButton4_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton4.CheckedChanged
+        ioperation = "LM"
+    End Sub
+
+    Private Sub RadioButton10_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton10.CheckedChanged
+        ioperation = "ML"
+    End Sub
+
+    Private Sub RadioButton9_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton9.CheckedChanged
+        ioperation = "KM"
+    End Sub
+
+    Private Sub RadioButton8_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton8.CheckedChanged
+        ioperation = "MK"
+    End Sub
+
+    Private Sub RadioButton7_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton7.CheckedChanged
+        ioperation = "MF"
+    End Sub
+
+    Private Sub RadioButton11_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton11.CheckedChanged
+        ioperation = "FI"
+    End Sub
+
+    Private Sub RadioButton12_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton12.CheckedChanged
+        ioperation = "CM"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Select Case (ioperation)
+            Case "KM"
+                'Kilometer to Meter
+                Kilometer = Double.Parse(txtConvert.Text)
+                lblConvert.Text = ((1000 * Kilometer).ToString())
+
+            Case "MK"
+                'Meter to Kilometer
+                Meter = Double.Parse(txtConvert.Text)
+                lblConvert.Text = ((Meter / 1000).ToString())
+
+            Case "MF"
+                'Meter to Feet
+                Inches = Double.Parse(txtConvert.Text)
+                lblConvert.Text = ((3.2808399 * Inches).ToString())
+
+            Case "FI"
+                'Feet to Inches
+                Feet = Double.Parse(txtConvert.Text)
+                lblConvert.Text = ((Feet * 12).ToString())
+
+            Case "KG"
+                'Kilogram to Gram
+                Kilo = Double.Parse(txtConvert.Text)
+                lblConvert.Text = ((1000 * Kilo).ToString())
+
+            Case "GK"
+                'Gram to Kilogram
+                Gram = Double.Parse(txtConvert.Text)
+                lblConvert.Text = ((Gram / 1000).ToString())
+
+            Case "LM"
+                'Liter to Mililiter
+                Liter = Double.Parse(txtConvert.Text)
+                lblConvert.Text = ((1000 * Liter).ToString())
+
+            Case "ML"
+                'Mililiter to Liter
+                Mililiter = Double.Parse(txtConvert.Text)
+                lblConvert.Text = ((Mililiter / 1000).ToString())
+
+            Case "CM"
+                'Centimeter to Meter
+                Centimeter = Double.Parse(txtConvert.Text)
+                lblConvert.Text = ((Centimeter / 100).ToString())
+            Case "CF"
+                'Celsius to Fahrenheit
+                Celsius = Double.Parse(txtConvert.Text)
+                lblConvert.Text = ((((9 * Celsius) / 5) + 32).ToString())
+
+            Case "FC"
+                'Fahrenheit to Celsius
+                Fahrenheit = Double.Parse(txtConvert.Text)
+                lblConvert.Text = ((((Fahrenheit - 32) * 5) / 9).ToString())
+
+            Case "K"
+                'Convert Celsius to Kelvin
+                Kelvin = Double.Parse(txtConvert.Text)
+                lblConvert.Text = (((((1 * Kelvin) / 1) + 1) + 272.15).ToString())
+        End Select
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        txtConvert.Clear()
+        lblConvert.Text = ""
+        RadioButton1.Checked = False
+        RadioButton2.Checked = False
+        RadioButton3.Checked = False
+        RadioButton4.Checked = False
+        RadioButton5.Checked = False
+        RadioButton6.Checked = False
+        RadioButton7.Checked = False
+        RadioButton8.Checked = False
+        RadioButton9.Checked = False
+        RadioButton10.Checked = False
+        RadioButton11.Checked = False
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub lblConvert_Click(sender As Object, e As EventArgs) Handles lblConvert.Click
+
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        ioperation = "K"
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        ioperation = "FC"
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        ioperation = "CF"
+    End Sub
+
+    Dim Kilometer, Meter, Inches, Feet As Double
+
+    Dim ioperation As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
